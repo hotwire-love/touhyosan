a1743da0a78b7c17d9afe7ea6a5938e2bca3dbe1
ichiroc <nobitahouse@gmail.com>
2022-09-29 19:22:22 +0900

    feat: Poll 作るところまで

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,12 @@
 class Poll < ApplicationRecord
   validates :title, presence: true
   has_many :choices, dependent: :destroy
+
+  def choices_text
+    choices.map(&:title).join("\n")
+  end
+
+  def choices_text=(text)
+    self.choices = text.split("\n").map { |title| choices.find_or_initialize_by(title:) }
+  end
 end
