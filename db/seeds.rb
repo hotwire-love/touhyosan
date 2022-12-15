c19ddc5922dff0154bc0db18a2f3cd46058192cd
ykominami <ykominami@gmail.com>
2022-12-15 16:43:32 +0900

    set seed date to VoteDetail

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ Vote.destroy_all
 Choice.destroy_all
 Poll.destroy_all
 # TODO: VoteDetailを作ったら削除するコードを追加
+VoteDetail.destroy_all
 
 poll = Poll.create!(title: 'Hotwire.love Vol.1のトークテーマ')
 poll.choices.create!(title: 'jQueryからHotwireに移行するためのノウハウが知りたい')
@@ -13,3 +14,12 @@ poll.votes.create!(user_name: 'いちろー', comment: '家の用事があるた
 poll.votes.create!(user_name: 'ひーくん')
 
 # TODO: VoteDetailを作ったら登録するコードを追加
+# app/models/vote_detail.rbでのstatusに対するenum定義は以下の通り
+#  yes: 0, yes_or_no: 1, no: 2
+statuses = [ [0, 1, 2], [1, 1, 0], [0, 2, 1] ]
+
+poll.votes.zip(statuses).each do |vote, statusx|
+  poll.choices.zip(statusx).each do |choice, status|
+    vote.vote_details.create(choice: choice, status: status)
+  end
+end
