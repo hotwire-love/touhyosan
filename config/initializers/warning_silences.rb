749a3fa29ab960337025f3d45014114cb84ee350
Yasumasa Ashida <ys.ashida@gmail.com>
2023-04-27 20:01:18 +0900

    rails db:migrate etcがwarningでおちるのを回避する

---
+++ config/initializers/warning_silences.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Silence warnings:
+
+# PG::Coder.new(hash) is deprecated. Please use keyword arguments instead!
+Warning.ignore(/PG::Coder.new\(hash\) is deprecated/)
