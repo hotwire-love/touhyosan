bf074a0ca7956ba9bd0434ddcdc183c03726a029
Junichi Ito <jit@sonicgarden.jp>
2024-08-28 20:05:47 +0900

    refactor: コメントの代わりにメソッド分割を実施

--- app/controllers/polls/choices_controller.rb
+++ app/controllers/polls/choices_controller.rb
@@ -11,8 +11,9 @@ module Polls
       @choice = @poll.choices.build(choice_params)
       if @choice.save
         Turbo::StreamsChannel.broadcast_append_to @poll, target: "poll-choices", partial: 'polls/choices/choice', locals: { choice: @choice }
-
-        broadcast_choice_change
+        broadcast_choice_change_to_new_votes
+        broadcast_choice_change_to_existing_votes
+        broadcast_choice_change_to_poll_result
       else
         render :index, status: :unprocessable_entity
       end
@@ -24,8 +25,9 @@ module Polls
     def update
       if @choice.update(choice_params)
         Turbo::StreamsChannel.broadcast_replace_to @poll, target: [@choice, 'poll-choice'], partial: 'polls/choices/choice', locals: { choice: @choice }
-
-        broadcast_choice_change
+        broadcast_choice_change_to_new_votes
+        broadcast_choice_change_to_existing_votes
+        broadcast_choice_change_to_poll_result
       else
         render :edit, status: :unprocessable_entity
       end
@@ -35,26 +37,32 @@ module Polls
       @choice.destroy
       # TODO: [@choice, 'poll-choice'] をうまく管理したい
       Turbo::StreamsChannel.broadcast_remove_to @poll, target: [@choice, 'poll-choice']
-
-      broadcast_choice_change
+      broadcast_choice_change_to_new_votes
+      broadcast_choice_change_to_existing_votes
+      broadcast_choice_change_to_poll_result
 
       # NOTE: ごくまれに0バイトのHTMLが返されることがあるので、それを防ぐために destroy.turbo_stream.erb を返す
     end
 
     private
 
-    def broadcast_choice_change
-      Turbo::StreamsChannel.broadcast_replace_to @poll, target: "poll_result", partial: 'polls/result', locals: { poll: @poll }
-
-      @poll.votes.each do |vote|
-        Turbo::StreamsChannel.broadcast_replace_to vote, target: 'vote_details', partial: 'polls/votes/vote_details', locals: { poll: @poll, vote: vote }
-      end
-
+    def broadcast_choice_change_to_new_votes
       new_vote = @poll.votes.new
       @poll.choices.each_with_index do |choice, index|
         new_vote.vote_details.build(choice: choice, position: index)
       end
       Turbo::StreamsChannel.broadcast_replace_to "#{@poll.id}-new-vote", target: 'vote_details', partial: 'polls/votes/vote_details', locals: { poll: @poll, vote: new_vote }
+    end
+
+    def broadcast_choice_change_to_existing_votes
+      @poll.votes.each do |vote|
+        next if vote.new_record?
+        Turbo::StreamsChannel.broadcast_replace_to vote, target: 'vote_details', partial: 'polls/votes/vote_details', locals: { poll: @poll, vote: vote }
+      end
+    end
+
+    def broadcast_choice_change_to_poll_result
+      Turbo::StreamsChannel.broadcast_replace_to @poll, target: "poll_result", partial: 'polls/result', locals: { poll: @poll }
     end
 
     def set_poll
