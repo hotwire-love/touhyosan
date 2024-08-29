ef1ec18dcdee9ca52e7d898177a7972207295f55
Junichi Ito <jit@sonicgarden.jp>
2024-08-29 10:35:20 +0900

    新規作成モーダルへのリアルタイム反映のテストを追加

--- spec/system/choices_spec.rb
+++ spec/system/choices_spec.rb
@@ -12,8 +12,32 @@ RSpec.describe 'Choices', :js, type: :system do
     end
   end
 
+  def assert_votes(*choices, user_name: '', comment: '')
+    wait_for_turbo
+    expect(page).to have_field 'User name', with: user_name unless user_name.empty?
+    expect(page).to have_field 'Comment', with: comment
+    votes = all('.vote-list li')
+    expect(votes.size).to eq choices.size
+    votes.each_with_index do |li, i|
+      within li do
+        expect(page).to have_content choices[i]
+      end
+    end
+  end
+
   def wait_for_turbo
     sleep 0.5
+  end
+
+  def create_existing_vote
+    poll = Poll.create!(title: 'Hotwire.love meetup Vol.35')
+    poll.choices.create!(title: 'Turboについて')
+    new_vote = poll.votes.new(user_name: "Alice")
+    poll.choices.each_with_index do |choice, index|
+      new_vote.vote_details.build(choice: choice, position: index)
+    end
+    new_vote.save!
+    new_vote
   end
 
   example 'CRUDができる' do
@@ -52,13 +76,8 @@ RSpec.describe 'Choices', :js, type: :system do
 
   describe '投票結果テーブルへのリアルタイム反映' do
     before do
-      @poll = Poll.create!(title: 'Hotwire.love meetup Vol.35')
-      @choice = @poll.choices.create!(title: 'Turboについて')
-      new_vote = @poll.votes.new(user_name: "Alice")
-      @poll.choices.each_with_index do |choice, index|
-        new_vote.vote_details.build(choice: choice, position: index)
-      end
-      new_vote.save!
+      vote = create_existing_vote
+      @poll = vote.poll
     end
 
     example '選択肢のCUDがリアルタイムに反映される' do
@@ -98,6 +117,53 @@ RSpec.describe 'Choices', :js, type: :system do
         end
       end
       assert_ranking('Stimulusについて')
+    end
+  end
+
+  describe '新規作成モーダルへのリアルタイム反映' do
+    before do
+      vote = create_existing_vote
+      @poll = vote.poll
+    end
+
+    example '選択肢のCUDがリアルタイムに反映される' do
+      visit poll_path(@poll)
+      click_link '投票を作成'
+      assert_votes('Turboについて')
+
+      # Create
+      using_session 'Bob' do
+        visit poll_choices_path(@poll)
+        expect(page).to have_selector 'h2', text: 'Hotwire.love meetup Vol.35'
+
+        fill_in '新しい選択肢', with: 'Stimulusについて'
+        click_button '登録する'
+        within '#poll-choices' do
+          expect(page).to have_selector 'li', text: 'Stimulusについて'
+        end
+      end
+      assert_votes('Turboについて', 'Stimulusについて')
+
+      # Update
+      using_session 'Bob' do
+        within '#poll-choices' do
+          click_link 'Turboについて'
+          expect(page).to have_field 'choice[title]', with: 'Turboについて'
+          fill_in 'choice[title]', with: 'About Turbo'
+          click_button '更新する'
+          expect(page).to have_selector 'li', text: 'About Turbo'
+        end
+      end
+      assert_votes('About Turbo', 'Stimulusについて')
+
+      # Delete
+      using_session 'Bob' do
+        accept_confirm { first('#poll-choices button').click }
+        within '#poll-choices' do
+          expect(page).to_not have_selector 'li', text: 'About Turbo'
+        end
+      end
+      assert_votes('Stimulusについて')
     end
   end
 end
