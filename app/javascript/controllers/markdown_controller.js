d1d4ed4c06e7695363ff9334a68b3205bc9b20c9
Junichi Ito <jit@sonicgarden.jp>
2025-04-16 20:15:50 +0900

    プレビューの切り替え機能を実装

--- app/javascript/controllers/markdown_controller.js
+++ app/javascript/controllers/markdown_controller.js
@@ -4,7 +4,7 @@ import DOMPurify from 'dompurify';
 
 // Connects to data-controller="markdown"
 export default class extends Controller {
-  static targets = ["editor", "preview"]
+  static targets = ["editor", "preview", "previewSwitch"]
   static values = {
     source: String
   }
@@ -12,6 +12,7 @@ export default class extends Controller {
   connect() {
     if (this.hasEditorTarget) {
       this.change()
+      this.togglePreview()
     } else {
       this.show()
     }
@@ -23,5 +24,10 @@ export default class extends Controller {
 
   show() {
     this.previewTarget.innerHTML = DOMPurify.sanitize(marked.parse(this.sourceValue));
+  }
+
+  togglePreview() {
+    this.editorTarget.classList.toggle("d-none", this.previewSwitchTarget.checked);
+    this.previewTarget.classList.toggle("d-none", !this.previewSwitchTarget.checked);
   }
 }
