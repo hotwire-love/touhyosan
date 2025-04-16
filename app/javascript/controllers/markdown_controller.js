e84a89b82affc79c0b7a6a5b8d780bc9e9337ecc
Junichi Ito <jit@sonicgarden.jp>
2025-04-16 20:02:49 +0900

    投票結果画面にもマークダウンのパース結果を表示

--- app/javascript/controllers/markdown_controller.js
+++ app/javascript/controllers/markdown_controller.js
@@ -4,12 +4,23 @@ import { marked } from "marked";
 // Connects to data-controller="markdown"
 export default class extends Controller {
   static targets = ["editor", "preview"]
+  static values = {
+    source: String
+  }
 
   connect() {
-    this.change()
+    if (this.hasEditorTarget) {
+      this.change()
+    } else {
+      this.show()
+    }
   }
 
   change() {
     this.previewTarget.innerHTML = marked.parse(this.editorTarget.value);
+  }
+
+  show() {
+    this.previewTarget.innerHTML = marked.parse(this.sourceValue);
   }
 }
